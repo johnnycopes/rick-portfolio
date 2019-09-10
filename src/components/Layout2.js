a99ba37c139c7b3ff21237828dc958317523c41d
John Coppola <jcoppola@insiten.com>
2019-09-10 13:25:00 -0400

    temp layout overwrite (not sure what's happening)

---
+++ src/components/Layout2.js
@@ -0,0 +1,27 @@
+import React from "react"
+import PropTypes from "prop-types"
+import cx from "classnames"
+
+import styles from "../styles/components/Layout.module.scss"
+import Header from "./Header"
+
+const Layout = ({ verticallyCentered, children }) => (
+  <>
+    <Header />
+    <div className={styles.wrapper}>
+      <div className={cx(
+        styles.content,
+        { [styles.verticallyCentered]: verticallyCentered }
+      )}>
+        {children}
+      </div>
+    </div>
+  </>
+)
+
+Layout.propTypes = {
+  verticallyCentered: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
