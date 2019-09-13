a420dd6efe8af4f60635d47eaf7875778bc7e261
John Coppola <jcoppola@insiten.com>
2019-09-12 20:53:57 -0400

    re-implements accidentally erased components

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import styles from "../styles/components/Button.module.scss"
+
+const Button = ({ children }) => (
+  <button className={styles.button}>
+    {children}
+  </button>
+)
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Button 
