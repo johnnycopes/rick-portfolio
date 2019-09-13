f69684be7fb9f5a48d73fecf33229f088aa6e06b
John Coppola <jcoppola@insiten.com>
2019-09-12 22:05:10 -0400

    prevents delete of About page in CMS; refactors About query to dynamically fetch selected resume

--- src/templates/about.js
+++ src/templates/about.js
@@ -7,47 +7,50 @@ import Layout from "../components/Layout"
 import Button from "../components/Button"
 import ExternalLink from "../components/ExternalLink";
 
-const AboutTemplate = ({ data }) => (
-  <Layout verticallyCentered={true}>
-    <div className={styles.about}>
-      <Img
-        className={styles.pic}
-        fluid={data.markdownRemark.frontmatter.image.childImageSharp.fluid}
-        alt="Rick Segal"
-      />
-      <div className={styles.bio}>
-        <h2 className={styles.greeting}>
-          Hey, it's me — Rick.
-        </h2>
-        <div className={styles.buttons}>
-          <ExternalLink link={data.file.publicURL}>
-            <Button>résumé</Button>
-          </ExternalLink>
-          <ExternalLink link="https://www.linkedin.com/in/rsegal">
-            <Button>linkedin</Button>
-          </ExternalLink>
-          <ExternalLink link="https://www.instagram.com/riiyaack">
-            <Button>instagram</Button>
-          </ExternalLink>
-          <ExternalLink link="https://www.wikipedia.org">
-            <Button>learn too much</Button>
+const AboutTemplate = ({ data }) => {
+  const about = data.markdownRemark.frontmatter
+  return (
+    <Layout verticallyCentered={true}>
+      <div className={styles.about}>
+        <Img
+          className={styles.pic}
+          fluid={about.image.childImageSharp.fluid}
+          alt="Rick Segal"
+        />
+        <div className={styles.bio}>
+          <h2 className={styles.greeting}>
+            Hey, it's me — Rick.
+          </h2>
+          <div className={styles.buttons}>
+            <ExternalLink link={about.resume.publicURL}>
+              <Button>résumé</Button>
+            </ExternalLink>
+            <ExternalLink link="https://www.linkedin.com/in/rsegal">
+              <Button>linkedin</Button>
+            </ExternalLink>
+            <ExternalLink link="https://www.instagram.com/riiyaack">
+              <Button>instagram</Button>
+            </ExternalLink>
+            <ExternalLink link="https://www.wikipedia.org">
+              <Button>learn too much</Button>
+            </ExternalLink>
+          </div>
+          <ExternalLink link="mailto:risegal4@gmail.com">
+            <h4>risegal4@gmail.com</h4>
           </ExternalLink>
         </div>
-        <ExternalLink link="mailto:risegal4@gmail.com">
-          <h4>risegal4@gmail.com</h4>
-        </ExternalLink>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query About($id: String!) {
-    file(extension: {eq: "pdf"}) {
-      publicURL
-    }
     markdownRemark(id: { eq: $id }) {
       frontmatter {
+        resume {
+          publicURL
+        }
         image {
           childImageSharp {
             fluid {
